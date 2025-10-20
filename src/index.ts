43fb7cf17bcc026ab39b8dd62bdf2dcb2d61b6a7
Björn <41452212+bjoern621@users.noreply.github.com>
2025-10-20 22:26:50 +0200

    Add plugin configuration for eslint-plugin-exhaustive-deps-exclude

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import ExhaustiveDeps from "./rules/ExhaustiveDeps";
+
+const plugin = {
+    rules: {
+        "exhaustive-deps": ExhaustiveDeps,
+    },
+    configs: {
+        recommended: {
+            plugins: ["exhaustive-deps-exclude"],
+            rules: {
+                "react-hooks/exhaustive-deps": "off",
+                "exhaustive-deps-exclude/exhaustive-deps": "error",
+            },
+        },
+    },
+};
+
+export default plugin;
